65f7f6e437e8fb28bdb35e5e6087225128b44637
Patrick Flor <git.push.origin@gmail.com>
2013-05-27 11:37:09 -0500

    Osteomorph teh skeleton; gotta do some house packing!
    Not sure this falls under the heading of ship it... but it's time to do
    real world things, and this thunderstorm might just nuke my computer!

--- skiplist/skiplist.go
+++ skiplist/skiplist.go
@@ -1,55 +1,91 @@
-// Package skiplist: a skiplist (slavishly) modeled on container/list
-// "Skip Lists: A Probabilistic Alternative to Balanced Trees", W.
-// Pugh, 1990.
-
+// Package skiplist: a skiplist (slavishly) modeled on container/list.
+// See Pugh, W. "Skip Lists: A Probabilistic Alternative to Balanced
+// Trees", W. Pugh, CACM, 33:6 (June 1990).
 package skiplist
 
 import (
+	//"container/list"
 	//"math/rand"
 )
 
+// Datum _cum_ forward skip iterator
 type Element struct {
-	Value   interface{}
-	next []*Element
-	parent  *Skiplist
+	next  []*Element
+	sl    *Skiplist
+	Value interface{}
 }
 
-func (e *Element) Next(level int) *Element {
-	if len(e.next) < level {
+// Iterate forward at a given skip level
+func (e *Element) Next(lev int) *Element {
+	if len(e.next) < lev || lev < 0 || e.next[lev] == &e.sl.root {
 		return nil
 	}
-	return e.next[level]
+	return e.next[lev]
+}
+
+// Finds the next non-root element at the highest possible level
+func (e *Element) SkipForward(max int) *Element {
+	for lev := len(e.next)-1; lev >= 0; lev-- {
+		if n := e.Next(lev); n != &e.sl.root {
+			return n
+		}
+	}
+	return nil 
 }
 
 type Skiplist struct {
 	root Element
-	len, height int
+	len int
 }
 	
-func (sl *Skiplist) Init() *Skiplist {
-	sl.root.next = []*Element{&sl.root}
-	sl.len = 0
-	sl.height = 0
-	return sl
-}
-
 func New() *Skiplist {
 	return new(Skiplist).Init()
 }
-	
-func (sl *Skiplist) Len() int {
-	return sl.len
-}
 
-// TODO: func (sl *Skiplist) Min() // easy
-//       func (sl *Skiplist) Max() // bit more involved, or tail
-//       ptr/cache?
+func (sl *Skiplist) Init() *Skiplist {
+	sl.root.next = []*Element{&sl.root}
+	sl.len = 0
+	return sl
+}
 
 func (sl *Skiplist) lazyInit() {
 	if len(sl.root.next) == 0 {
 		sl.Init()
 	}
 }
-
-// TODO: func (sl *Skiplist) insert(e, at *Element) *Element {
 	
+func (sl *Skiplist) Len() int {
+	return sl.len
+}
+
+func (sl *Skiplist) Min() *Element {
+	n := sl.root.Next(0)
+	if n == &sl.root {
+		return nil
+	}
+	return n;
+}
+// TODO: func (sl *Skiplist) Max() // bit more involved, or tail
+//       ptr/cache?
+
+// STUB
+func (sl *Skiplist) insert(e, at *Element) *Element {
+        //height := randomLevel(sl.len)
+	//if len(sl.next)
+	return e 
+}
+
+// STUB
+func randomLevel(len int) int {
+	return 4 
+}
+
+// TODO: factor max out into some kind of util package. I really miss
+// generics/ternary ops here; what's the non-casting alternative? c.f.
+// https://groups.google.com/forum/?fromgroups=#!topic/golang-nuts/auLWUqourlA%5B1-25-false%5D
+func max(x, y int) int {
+	if x >= y {
+		return x
+	}
+	return y
+}
